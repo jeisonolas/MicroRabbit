2014b78a73984c448584a16d0a5a9a0c9fea1aa1
Jeik <jeisonolas300@gmail.com>
2020-04-14 12:10:58 -0500

    Partially Completed Banking Microservice

---
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _service;
+
+        public AccountService(IAccountRepository service)
+        {
+            _service = service;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _service.GetAccounts();
+        }
+    }
+}
